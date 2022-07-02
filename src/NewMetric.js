42aad9e37156359c033a558880103b3e0fbd5797
Paulo Phagula <PauloPhagula@users.noreply.github.com>
2022-07-02 02:26:24 +0200

    Add eslint and format code with it

--- src/NewMetric.js
+++ src/NewMetric.js
@@ -1,41 +1,61 @@
-import React, { useState, useEffect, useRef }  from 'react'
-import { Link } from "react-router-dom";
-import axios from 'axios';
-import { useForm } from "react-hook-form";
-import { Navigate } from 'react-router-dom';
-import moment from "moment";
+import React, { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { Navigate } from 'react-router-dom'
+import axios from 'axios'
+import moment from 'moment'
+import _ from 'lodash';
 
-const API_BASE_URL = "http://127.0.0.1:3000/api/v1"
-const defaultDate = moment().format("YYYY-MM-DDThh:mm");
+const API_BASE_URL = 'http://127.0.0.1:3000/api/v1'
+const defaultDate = moment().format('YYYY-MM-DDThh:mm')
 
 function NewMetric() {
-  const [submittedSuccessfuly, setSubmittedSuccessfuly] = useState(null);
-  const [showSubmitWarn, setShowSubmitWarn] = useState(false);
+  const [metricNames, setMetricNames] = useState(['all']);
+  const [hasLoadedMetricNames, setHasLoadedMetricNames] = useState(false);
 
-
-  const {register, handleSubmit} = useForm()
-  const onSubmit = (formData) => {
-    const params = {
-      "metric": { ...formData }
+  useEffect(() => {
+    if (hasLoadedMetricNames === true) {
+      return;
     }
 
     axios
-      .post(API_BASE_URL + "/metrics", params)
-      .then((response) => {setSubmittedSuccessfuly(true)})
-      .catch((err) => {setSubmittedSuccessfuly(false)});
-  };
+      .get(API_BASE_URL + "/metric_names")
+      .then((response) => {
+        setMetricNames((prevMetricNames) => _.uniq([...response.data, ...prevMetricNames]));
+        setHasLoadedMetricNames(true);
+      }).catch((err) => {
+        // Swallow it. This request is not critical. It's data is just for suggestions.
+      });
+  }, [metricNames, hasLoadedMetricNames]);
+
+  const [submittedSuccessfuly, setSubmittedSuccessfuly] = useState(null)
+  const [showSubmitWarn, setShowSubmitWarn] = useState(false)
+
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (formData) => {
+    const params = {
+      metric: { ...formData },
+    }
+
+    axios
+      .post(API_BASE_URL + '/metrics', params)
+      .then((response) => {
+        setSubmittedSuccessfuly(true)
+      })
+      .catch((err) => {
+        setSubmittedSuccessfuly(false)
+      })
+  }
 
   useEffect(() => {
     if (submittedSuccessfuly === false) {
-      setShowSubmitWarn(true);
+      setShowSubmitWarn(true)
     } else {
-      setShowSubmitWarn(false);
+      setShowSubmitWarn(false)
     }
     return () => {
-      setShowSubmitWarn(false);
-    };
-  }, [submittedSuccessfuly]);
-
+      setShowSubmitWarn(false)
+    }
+  }, [submittedSuccessfuly])
 
   return (
     <>
@@ -44,51 +64,73 @@ function NewMetric() {
       <div className="container">
         <div className="row justify-content-center">
           <form className="form col-4" onSubmit={handleSubmit(onSubmit)}>
-
             <div className="mb-3">
-              <label htmlFor="name" className="form-label">Name</label>
-              <input type="text" className="form-control" aria-describedby="nameHelp" list="defaultMetricNames"
-                {...register("name", { required: true })}
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                aria-describedby="nameHelp"
+                list="defaultMetricNames"
+                {...register('name', { required: true })}
               />
-              <div id="name" className="form-text">What was measured</div>
+              <div id="name" className="form-text">
+                What was measured
+              </div>
               <datalist id="defaultMetricNames">
-                <option value="cpu_usage" />
+                {metricNames.map((name) => <option value={name} key={name} /> )}
               </datalist>
             </div>
 
             <div className="mb-3">
-              <label htmlFor="value" className="form-label">Value</label>
-              <input type="number" className="form-control" aria-describedby="valueHelp"
-                {...register("metric_value", { required: true })}
+              <label htmlFor="value" className="form-label">
+                Value
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                aria-describedby="valueHelp"
+                {...register('metric_value', { required: true })}
               />
-              <div id="valueHelps" className="form-text">How much it measured.</div>
+              <div id="valueHelps" className="form-text">
+                How much it measured.
+              </div>
             </div>
 
             <div className="mb-3">
-              <label htmlFor="timepoint" className="form-label">Time</label>
-              <input type="datetime-local" className="form-control" aria-describedby="timepoint"
-                {...register("timepoint", { required: true, max: defaultDate })}
+              <label htmlFor="timepoint" className="form-label">
+                Time
+              </label>
+              <input
+                type="datetime-local"
+                className="form-control"
+                aria-describedby="timepoint"
+                {...register('timepoint', { required: true, max: defaultDate })}
               />
-              <div id="timepointHelp" className="form-text">When was it measured.</div>
+              <div id="timepointHelp" className="form-text">
+                When was it measured.
+              </div>
             </div>
 
             <div className="mb-3 mt-4 d-flex justify-content-center">
               <button type="submit" className="btn btn-primary d-block" id="saveButton">
-                  <i className="bi bi-plus" role="img" aria-label="Save"></i> Save
-                </button>
+               <i className="bi bi-plus" role="img" aria-label="Save"></i> Save
+              </button>
             </div>
 
-            {showSubmitWarn ?
+            {showSubmitWarn ? (
               <div className="alert alert-warning" role="alert">
                 An error happened submit this metric. Please try again!
               </div>
-              : <span/>
-            }
+            ) : (
+              <span />
+            )}
           </form>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default NewMetric;
+export default NewMetric
