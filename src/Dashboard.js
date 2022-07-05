c09da78185ea601b4548a3c3d75e65f6699f294a
Paulo Phagula <PauloPhagula@users.noreply.github.com>
2022-07-05 13:02:22 +0200

    Remove chart.js lib and unused useRef

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef }  from 'react'
+import React, { useState, useEffect }  from 'react'
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 import { DateTime } from "luxon";
